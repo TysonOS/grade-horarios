7c00a07aeec3b324e2fb6c1827143ccc0083536b
TysonOS <tysonoliveira2013@gmail.com>
2024-03-07 22:54:56 -0300

    feat persistent data/autosave and load

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 let locked = false;
+$(document).ready(function(){
+    timeTableLoad();
+    subjectListLoad();
+    dragNDropHandler();
+})
+
 const dias = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"];
 const horarios = {
     1: ["13:15", "14:05"],
@@ -13,72 +19,90 @@ const horarios = {
     10: ["21:10", "22:00"]
 };
 
-let divSubjectList = $("#subject-list");
-
-dias.forEach(dia => {
-    $("#class-schedule thead tr").append(`<th scope="col">${dia}</th>`).addClass("text-center table-success");
-});
-
-for (key in horarios) {
-    $("#class-schedule tbody")
-        .append(`<tr>
-                    <th class="table-success" scope="row">
-                        <time>${horarios[key][0]}</time> - 
-                        <time>${horarios[key][1]}</time>
-                    </th>
-                </tr>`);
+function dragNDropHandler(){
+    let dragged=null;
+    $("#class-schedule").delegate("td", "dragover", function(ev){
+        ev.preventDefault();
+    }).delegate("td","dragenter", function(){
+        $(this).toggleClass("table-warning", true)
+    }).delegate("td","dragleave", function(){
+        $(this).toggleClass("table-warning", false)
+    })
+    $("#subject-list, #class-schedule").delegate("[draggable='true']","dragstart", function(){
+        dragged = $(this);
+    })
+    $("#class-schedule").delegate("td", "drop", function(ev){
+        ev.preventDefault();
+        if(!locked){
+            if(dragged.is("li")){
+                $(this).replaceWith(draggedHandler(dragged));
+            }else{
+                $(this).replaceWith(dragged.clone());
+            }    
+        }
+        save();
+        $(this).toggleClass("table-warning", false)
+    })
 }
-dias.forEach(dia => {
-    $("#class-schedule tbody tr").append(`<td> - </td>`).addClass("text-center");
-});
 
-$.getJSON("materias.json", (subjects) => {
-    $.each(subjects, (sem, subjs) => {
-        tmpSemID = `s${sem}`;
-        $(` <buttton data-bs-target="#${tmpSemID}" class="btn" type="button" data-bs-toggle="collapse">
-                <h5>${!isNaN(sem)?"Semestre":""} ${sem}</h5>
-             </button>`).appendTo(divSubjectList)
+function timeTableLoad(){
+    if(localStorage.class_timetable){
+        loadData();
+    }else{
+        dias.forEach(dia => {
+            $("#class-schedule thead tr").append(`<th scope="col">${dia}</th>`).addClass("text-center table-success");
+        });
+        for (key in horarios) {
+            $("#class-schedule tbody")
+            .append(`<tr>
+            <th class="table-success" scope="row">
+            <time>${horarios[key][0]}</time> - 
+                                <time>${horarios[key][1]}</time>
+                            </th>
+                            </tr>`);
+                        }
+        dias.forEach(dia => {
+            $("#class-schedule tbody tr").append(`<td> - </td>`).addClass("text-center");
+        });
+    }
+}
 
-        $(`<div class="collapse" id="${tmpSemID}"></div>`).appendTo(divSubjectList);
-        let tmpSem = $(`#${tmpSemID}`);
-        $.each(subjs, (cod, subject) => {
-            $(`
+function subjectListLoad(){
+    let divSubjectList = $("#subject-list");
+    $.getJSON("materias.json", (subjects) => {
+        $.each(subjects, (sem, subjs) => {
+            tmpSemID = `s${sem}`;
+            $(` <buttton data-bs-target="#${tmpSemID}" class="btn" type="button" data-bs-toggle="collapse">
+            <h5>${!isNaN(sem)?"Semestre":""} ${sem}</h5>
+                 </button>`).appendTo(divSubjectList)
+    
+            $(`<div class="collapse" id="${tmpSemID}"></div>`).appendTo(divSubjectList);
+            let tmpSem = $(`#${tmpSemID}`);
+            $.each(subjs, (cod, subject) => {
+                $(`
                 <li id=${cod} draggable="true" class="list-group-item list-group-item-action"> 
                 ${subject.title}
                 </li>`
-            ).appendTo(tmpSem);
+                ).appendTo(tmpSem);
+            })
         })
     })
-})
-
-let dragged=null;
-
-$("#class-schedule").delegate("td", "drop", function(ev){
-    ev.preventDefault();
-    console.log("dropzone:", $(this))
-    if(!locked){
-        if(dragged.is("li")){
-            $(this).replaceWith(draggedHandler(dragged));
-        }else{
-            $(this).replaceWith(dragged.clone());
-        }    
-    }
-    $(this).toggleClass("table-warning", false)
-})
-
-$("#class-schedule").delegate("td", "dragover", function(ev){
-    ev.preventDefault();
-    console.log($(this));
-}).delegate("td","dragenter", function(){
-    $(this).toggleClass("table-warning", true)
-}).delegate("td","dragleave", function(){
-    $(this).toggleClass("table-warning", false)
-})
     
-$("#subject-list, #class-schedule").delegate("[draggable='true']","dragstart", function(){
-        dragged = $(this);
-})
+}
 
 function draggedHandler(draggedEl){
     return($(`<td draggable="true">${draggedEl.text().trim()}</td>`).addClass((draggedEl.attr("id").replace(/\d+/g, ''))));
+}
+
+function save(){
+    localStorage.setItem("class_timetable", $("#class-schedule")[0].innerHTML.trim());
+}
+function loadData(){
+    let timeTable = $(localStorage.getItem("class_timetable"));
+    $("#class-schedule").html(timeTable)
+}
+
+function purgeData(){
+    let timeTable = $(localStorage.removeItem("class_timetable"));
+    window.location.load();
 }
